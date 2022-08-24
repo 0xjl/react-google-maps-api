06d8baa6de706d51ce5a8af3d3931215e94ffae9
0xjdl <jdl.lourens@gmail.com>
2022-08-24 20:43:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const options = {
 
 export default function App() {
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyA23cu2DVISkxtHJR47WKzH4lC6pBURwBM',
+    googleMapsApiKey: AIzaSyA23cu2DVISkxtHJR47WKzH4lC6pBURwBM,
     libraries,
   });
   const [markers, setMarkers] = React.useState([]);
